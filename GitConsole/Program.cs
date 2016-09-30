be4445280eb4e12e4c742555fc807f11d0beb540
Dani <Dani@K>
2016-09-30 20:14:30 +0300

    Add project files.

---
+++ GitConsole/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("run test\n\r test2");
+
+            var input = Console.ReadLine();
+
+            Console.WriteLine(input);
+        }
+    }
+}
